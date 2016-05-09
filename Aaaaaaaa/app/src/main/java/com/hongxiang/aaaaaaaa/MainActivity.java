a6836798eb9c64f15f268ebe08d9e570025e3141
hongxiang <916634913@qq.com>
2016-05-09 10:49:05 +0800

    Second

--- Aaaaaaaa/app/src/main/java/com/hongxiang/aaaaaaaa/MainActivity.java
+++ Aaaaaaaa/app/src/main/java/com/hongxiang/aaaaaaaa/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     /**
      * 第二次练习啊 哈哈哈哈哈哈哈
+     * 啥地方还是对房价快速减肥的说法是对方考虑是否能
      * @param savedInstanceState
      */
     @Override
@@ -14,4 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     //heiehehieiehiehe0lofhaksdh;阿斯蒂芬安慰和人类和方法妨碍和发放环节ahfd'
+    //暗杀快点哈空间上是否还考试的计划覆盖爱的时刻了afa发爱上对方会就爱上短发爱上了快点放假啊放假
+    //水电费就喀什地方
+    //安峰撒旦法jasdf是d风格adsf安峰dfkgsfnasdf按价格大概会拉丝机房里看见男人看来你撒的发哦口岸无法j'asdfg 啊'
 }
