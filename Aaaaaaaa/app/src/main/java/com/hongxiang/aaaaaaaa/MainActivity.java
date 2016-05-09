05b9f9ad951e616ce949bca24044f9df6bb7cec3
hongxiang <916634913@qq.com>
2016-05-09 10:46:35 +0800

    第一次的提交

---
+++ Aaaaaaaa/app/src/main/java/com/hongxiang/aaaaaaaa/MainActivity.java
@@ -0,0 +1,17 @@
+package com.hongxiang.aaaaaaaa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    /**
+     * 第二次练习啊 哈哈哈哈哈哈哈
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //heiehehieiehiehe0lofhaksdh;阿斯蒂芬安慰和人类和方法妨碍和发放环节ahfd'
+}
